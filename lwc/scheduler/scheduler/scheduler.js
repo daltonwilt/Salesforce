4f19b59f09d3b90824b5d12d506597a51de06d70
daltonwilt <48796132+daltonwilt@users.noreply.github.com>
2024-11-22 17:12:48 -0500

    Add files via upload

--- lwc/scheduler/scheduler/scheduler.js
+++ lwc/scheduler/scheduler/scheduler.js
@@ -1,24 +1,3 @@
-import { LightningElement, api } from 'lwc';
-import { notification } from 'c/utility';
+import { LightningElement } from 'lwc';
 
-// Apex Imports
-import getCustomers from '@salesforce/apex/GenericService.getRecordsByType';
-
-export default class Dms_scheduler extends LightningElement {
-    customer;
-    customers;
-
-    constructor() {
-        super();
-    }
-
-    connectedCallback() {
-        getCustomers({objectType: 'dms_Customer__c', filter: null, orderBy: null}).then(gCustomers => {
-            this.customers = gCustomers;
-            console.log('Dms_customerSearch::getCustomers ', JSON.stringify(customers));
-
-        }).catch(error => {
-            console.error('Dms_customerSearch::getCustomers ', JSON.stringify(error));
-        })
-    }
-}
+export default class Scheduler extends LightningElement {}
