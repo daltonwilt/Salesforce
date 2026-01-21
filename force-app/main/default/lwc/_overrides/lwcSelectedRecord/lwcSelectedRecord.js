1a909f95b8e048f2356e5f0630c3cecc9af53e1a
daltonwilt <48796132+daltonwilt@users.noreply.github.com>
2026-01-21 16:02:20 -0500

    Update author information in lwcSelectedRecord.js

--- force-app/main/default/lwc/_overrides/lwcSelectedRecord/lwcSelectedRecord.js
+++ force-app/main/default/lwc/_overrides/lwcSelectedRecord/lwcSelectedRecord.js
@@ -1,9 +1,8 @@
 /**
  * @description       :
- * @author            : dalton@bluecitystudios.com
- * @group             :
+ * @author            : dalton@outlook.com
  * @last modified on  : 10/3/2024
- * @last modified by  : dalton@bluecitystudios.com
+ * @last modified by  : dalton@outlook.com
  **/
 import { LightningElement, api } from 'lwc';
 
